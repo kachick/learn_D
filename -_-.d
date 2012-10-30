a525eb14c1e20f518a5a0861a61c1bac8f6f001e
Kenichi Kamiya <kachick1@gmail.com>
2012-10-30 14:53:15 +0900

    add sample

---
+++ -_-.d
@@ -0,0 +1,11 @@
+import std.stdio;
+import std.string;
+
+void main() {
+  writeln("Input this mark: -_-");
+  char[] input;
+  while (chomp( readln() ).dup != "-_-")
+  {
+    writeln(">_<");
+  }
+}
---
+++ echo.d
@@ -0,0 +1,8 @@
+import std.stdio;
+
+void main() {
+  char[] input;
+  writeln("-_-");
+  input = readln().dup;
+  writeln(input);
+}
